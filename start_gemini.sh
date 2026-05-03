faf82e6f5863c7d02fa834559f00c8cfd1c9b5eb
Brent Marquez <brentonmarquez@gmail.com>
2026-05-02 22:14:20 -0600

    policies

--- start_gemini.sh
+++ start_gemini.sh
@@ -18,6 +18,9 @@ fi
 
 code .
 
+sbx policy allow network generativelanguage.googleapis.com
+sbx policy allow network gemini-api-docs-mcp.dev
+sbx policy allow network ai.google.dev
 sbx policy allow network oauth2.googleapis.com
 sbx policy allow network accounts.google.com
 sbx policy allow network play.googleapis.com
